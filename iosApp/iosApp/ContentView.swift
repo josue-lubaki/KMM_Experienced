52d1f6366c86cb64fe3cafed07e5a59b4af074fa
Josue Lubaki <josue.lubaki@alithya.com>
2023-05-08 17:34:36 -0400

    movie app done

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -6,6 +6,8 @@ struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         Main_iosKt.MainViewController()
     }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
---
+++ shared/src/commonMain/kotlin/ca/josue_lubaki/kmmexperiments/util/network/DataState.kt
@@ -0,0 +1,10 @@
+package ca.josue_lubaki.kmmexperiments.util.network
+
+/**
+* created by Josue Lubaki
+* date : 2023-05-08
+* version : 1.0.0
+*/
+
+class DataState {
+}
